7bcc913daef374f823a8b96b0049026182afd1f6
hang <isixline@qq.com>
2023-06-19 13:58:49 +0800

    feat: add serpapi

--- lang-chain/question-answerer.py
+++ lang-chain/question-answerer.py
@@ -1,11 +1,18 @@
 from langchain.llms import OpenAI
-import os
+from langchain.agents import AgentType, initialize_agent, load_tools
 from dotenv import load_dotenv
-
 load_dotenv()
 
-llm = OpenAI(temperature=0.9)
 
-text = "What would be a good company name for a company that makes colorful socks?"
-print(llm(text))
+# The language model we're going to use to control the agent.
+llm = OpenAI(temperature=0)
 
+# The tools we'll give the Agent access to. Note that the 'llm-math' tool uses an LLM, so we need to pass that in.
+tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+agent = initialize_agent(
+    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+# Let's test it out!
+agent.run("What was the high temperature in SF yesterday in Fahrenheit? What is that number raised to the .023 power?")
